d81d0f63eaf86dd56a1f26a8ab066a08b5a5e354
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-04-19 07:28:43 +0530

    Test Max

---
+++ src/com/testmax/Main.java
@@ -0,0 +1,15 @@
+package com.testmax;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Integer[] i = {56,30,70};
+        String[] s = {"Peach","Banana","Apple"};
+        Float[] f = {56f,30f,70f};
+        new MaxTest(i).maxElement();
+        new MaxTest(s).maxElement();
+        new MaxTest(f).maxElement();
+    }
+}
+
---
+++ src/com/testmax/MaxTest.java
@@ -0,0 +1,22 @@
+package com.testmax;
+
+public class MaxTest<T extends Comparable<T>>  {
+    private T[] inputArray;
+
+    MaxTest(T[] inputArray){
+        this.inputArray = inputArray;
+    }
+
+    public void  maxElement() {
+        for(int i=0; i<inputArray.length-1; i++) {
+            if(inputArray[i].compareTo(inputArray[i+1]) > 0) {
+                inputArray[i+1] = inputArray[i];
+            }
+        }
+        printMax();
+    }
+
+    private void printMax() {
+        System.out.println(inputArray[inputArray.length - 1]);
+    }
+}
